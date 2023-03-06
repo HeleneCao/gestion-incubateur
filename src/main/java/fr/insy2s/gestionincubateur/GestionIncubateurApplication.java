291475e043704f067a515367f070df73ea816689
Helene <helenecao59@gmail.com>
2023-03-06 16:09:00 +0100

    Version initial

---
+++ src/main/java/fr/insy2s/gestionincubateur/GestionIncubateurApplication.java
@@ -0,0 +1,13 @@
+package fr.insy2s.gestionincubateur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionIncubateurApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionIncubateurApplication.class, args);
+	}
+
+}
