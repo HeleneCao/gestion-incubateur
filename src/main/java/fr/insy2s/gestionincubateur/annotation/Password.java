853604f85cfbdb73bcb8f88caa6e99fab293afcd
peter.mollet <peter.mollet@neuf.fr>
2023-03-10 15:53:48 +0100

    [FIX][PM] - fix sequences in Entities, so that liquibase:diff work properly

--- src/main/java/fr/insy2s/gestionincubateur/annotation/Password.java
+++ src/main/java/fr/insy2s/gestionincubateur/annotation/Password.java
@@ -3,7 +3,11 @@ package fr.insy2s.gestionincubateur.annotation;
 
 import fr.insy2s.gestionincubateur.annotation.validator.PasswordConstraintsValidator;
 import jakarta.validation.Constraint;
-import java.lang.annotation.*;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
--- src/main/java/fr/insy2s/gestionincubateur/entity/Skill.java
+++ src/main/java/fr/insy2s/gestionincubateur/entity/Skill.java
@@ -1,13 +1,14 @@
 package fr.insy2s.gestionincubateur.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.util.List;
 
 @Data
 @Entity
